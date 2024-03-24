918f4bbe13a6d36e9dd123efe0ee80a21533b32a
Franmb93 <fcomunozbetanzos@gmail.com>
2024-03-24 12:44:45 +0100

    [Cloud-gateway] Initial commit cloud gateway

---
+++ paddle-cloud/src/main/java/com/fmb/paddlecloud/PaddleCloudApplication.java
@@ -0,0 +1,13 @@
+package com.fmb.paddlecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddleCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaddleCloudApplication.class, args);
+	}
+
+}
